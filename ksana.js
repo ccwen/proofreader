2bccdbbfc021107e163dfa49267fa2091e484e5c
yapcheahshen <yapcheahshen@gmail.com>
2016-06-29 14:43:38 +0800

    dn1 with image ok

---
+++ ksana.js
@@ -0,0 +1,41 @@
+jsonp_handler({
+ "version": "1",
+ "build": 63,
+ "title": "proofreader",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/ksanaforge/proofreader/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "react-dom",
+   "react-addons-update",
+   "react-addons-pure-render-mixin"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "codemirror.css",
+  "react-bundle.js",
+  "bundle.js",
+  "ksana.js"
+ ],
+ "filesizes": [
+  516,
+  157,
+  8006,
+  672371,
+  453903,
+  726
+ ],
+ "filedates": [
+  "2016-06-29T05:48:57.361Z",
+  "2015-10-27T10:19:29.200Z",
+  "2016-06-29T06:04:38.008Z",
+  "2015-10-27T09:36:14.497Z",
+  "2016-06-29T06:40:23.434Z",
+  "2016-06-29T06:38:16.100Z"
+ ],
+ "date": "2016-06-29T06:40:24.369Z"
+})
